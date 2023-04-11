ca301050c38f11c551601be1c2c7ce50cdeb81e2
Isaykin Vasily <v.chaser@mail.ru>
2023-04-11 17:12:22 +0300

    добавил билд бд на маке добавил поддержку идешками

--- front/bin/build_db.dart
+++ front/bin/build_db.dart
@@ -6,10 +6,23 @@ void main(List<String> args) async {
   const command = r'flutter packages pub run build_runner build';
 
   final result = await Process.run(
-    'powershell',
-    [command],
+    'zsh',
+    ['-c', command],
   );
 
   print('stdout:');
   print(result.stdout.toString());
+
+  print('stderr:');
+  print(result.stderr.toString());
+}
+
+Future<ProcessResult> runOnPlatform(String command) {
+  if (Platform.isWindows) {
+    return Process.run('powershell', [command]);
+  } else if (Platform.isMacOS) {
+    return Process.run('zsh', ['-c', command]);
+  } else {
+    throw Exception('Unsupported platform');
+  }
 }
